b1c7cbaf68a9cea8a971e278fb0d1185146b5ee0
Pablo Miro <paramiru@gmail.com>
2022-01-31 15:21:16 +0000

    Fixes errors in sampling algorithm

--- project/sampling_algorithm.py
+++ project/sampling_algorithm.py
@@ -2,8 +2,8 @@ import numpy as np
 
 class SamplingAlgorithm():
     def __init__(self, fourier_coeff): 
-        self.N = np.log2(fourier_coeff)
-        if self.N % 2 != 0:
+        self.N = np.log2(len(fourier_coeff))
+        if self.N % 1 != 0:
             raise ValueError("fourier_coeff should be an array of length power of 2")
         self.fourier_coeff = fourier_coeff
 
@@ -31,29 +31,29 @@ class SamplingAlgorithm():
         correlators = self.__get_correlators(y, self.fourier_coeff, VERBOSE)
         return 0.5 * (prob_limit + sum(correlators)/(2**k))
 
-    def sample_random_circuit(self, fc, N):
-        if fc % 2 != 0:
-            raise ValueError("Invalid Fourier Coefficients")
-        elif N > np.log2(len(fc)):
-            raise ValueError("N cannot be bigger than the size of the circuit")
+    def sample_random_circuit(self, VERBOSE):
+        # if N > np.log2(len(self.fourier_coeff)):
+        #     raise ValueError("N cannot be bigger than the size of the circuit")
         outcome = ''
         prob_limit = 1 # p(0) + p(1) = 2p_hat(0)
-        prob_add_zero = fc[0]
+        prob_add_zero = self.fourier_coeff[0]
         # N is length of bitstrings. Add check for N to be <= size of qubits in the circuit
-        for _ in range(N):
+        for _ in range(self.N.astype(int)):
             if np.random.default_rng().uniform(low=0, high=prob_limit) <= prob_add_zero:
                 outcome += '0'
                 prob_limit = prob_add_zero
             else:
                 outcome += '1'
                 prob_limit = 1 - prob_add_zero
-            prob_add_zero = self.__get_prob_add_zero(prob_limit, outcome)
+            prob_add_zero = self.__get_prob_add_zero(outcome, prob_limit, VERBOSE)
         return outcome
     
-    def sample_events(self, N: int, k: int):
+    def sample_events(self, N: int, k: int, VERBOSE=False):
         # TODO: remove hardcoded values with global variables 
         if (k > 23):
             raise ValueError("k is {}, but it must be smaller or equal to {},\
             which corresponds to the number of qubits in the circuit")
-        for _ in range(N):
-            self.sample_random_circuit(self.fourier_coeff)
+        print()
+        for i in range(1,N+1):
+            sample = self.sample_random_circuit(VERBOSE)
+            print(f"Outcome {i} is \'{sample}\'")
